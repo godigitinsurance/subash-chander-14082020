4db75412d79499cb29b82f46a460b9f1f5e0b436
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-13 20:27:38 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Subash Chander");
+$("#policyNumber").text("NJ01104193");
+$("#startDate").text("13-Aug-2020");
+$("#endDate").text("14-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB07AE6659");
+$("#make").text("HERO HONDA");
+$("#model").text("SPLENDOR PRO");
+$("#vehicalSubType").text("KICK START");
+$("#engineNo").text("HA10EHBSH13807");
+$("#chassis").text("MBLHA10ADB9H20234");
